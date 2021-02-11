22bbecb9c5eafcfe2d00537597f79eb08c451721
MaliniUI <malinibe2010@gmail.com>
2021-02-11 14:40:14 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -11,10 +11,31 @@ interface Reducer<T> {
 }
 
 let reducer: Reducer<number> = (state: number, action: Action) => {
-  return state;
+  switch (action.type) {
+    case "up":
+      return state + 1;
+    case "down":
+      return state - 1;
+    case "build":
+      return state + action.payload;
+    case "middle":
+      return state;
+    default:
+      return state;
+  }
 };
 
-console.log(reducer(0, null)); // -> 0
+let goUp: Action = { type: "up" };
+let goDown: Action = { type: "down" };
+let middle: Action = { type: "middle" };
+let build: Action = { type: "build", payload: 3 };
+
+console.log("go Up >> " + reducer(0, goUp));
+
+console.log("go Down >> " + reducer(0, goDown));
+
+console.log("Stay there >> " + reducer(0, middle));
+console.log("Build here >> " + reducer(3, build));
 
 const source = of("World").pipe(map(x => `Hello ${x}!`));
 
